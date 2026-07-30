97b82ef23e49f908c93c7697599d53a7e2be9d10
KarpX <carpeev.maksimka@yandex.ru>
2026-07-30 21:24:34 +0300

    feat: master-settings with the first run

--- builders.py
+++ builders.py
@@ -1,4 +1,4 @@
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 from enums import SETTINGS, MainMenuButtons, SettingsButtons
@@ -26,7 +26,7 @@ def build_inline_keyboard(list_of_buttons, adjust: list[int] = None):
     return inline_builder.as_markup()
 
 
-def build_inline_settings_keyboard(user_settings: dict, category: str, adjust: list[int] = None):
+def build_inline_settings_keyboard(user_settings: dict, category: str, adjust: list[int] = None, next_step: str = None):
     inline_builder = InlineKeyboardBuilder()
 
     current_category_items = SETTINGS[category]  # Получаем словарь с кнопками для текущей категории
@@ -38,6 +38,12 @@ def build_inline_settings_keyboard(user_settings: dict, category: str, adjust: l
             inline_builder.button(text=f"✅ {button['text']}", callback_data=button["callback_data"])
         else:
             inline_builder.button(text=f"❌ {button['text']}", callback_data=button["callback_data"])
+
+    if next_step:
+        inline_builder.row(InlineKeyboardButton(
+            text="Далее ➡️",
+            callback_data=f"next_step:{next_step}")
+        )
 
     if adjust is not None:
         inline_builder.adjust(*adjust)
@@ -63,25 +69,27 @@ def get_settings(is_active: bool = True):
     ], adjust=[3, 2, 1])
 
 
-def get_timezone_keyboard(current_start: int) -> InlineKeyboardMarkup:
+def get_timezone_keyboard(current_start: int, is_setup: bool = False) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
+
+    setup_flag = 1 if is_setup else 0
     
     # Создаем 6 кнопок с часовыми поясами (например, от -1 до 4)
     for tz in range(current_start, current_start + 6):
         # Ограничиваем реальными поясами (от -12 до +14)
         if -12 <= tz <= 14:
-            builder.button(text=f"{'+' if tz > 0 else ''}{tz}", callback_data=f"set_tz:{tz}")
+            builder.button(text=f"{'+' if tz > 0 else ''}{tz}", callback_data=f"set_tz:{tz}:{setup_flag}")
     
 
     # Добавляем кнопки навигации (стрелки)
     nav_buttons = []
     # Если мы не в самом начале, показываем стрелку влево
     if current_start > -12:
-        nav_buttons.append(builder.button(text="⬅️", callback_data=f"shift_tz:{current_start - 3}"))
+        nav_buttons.append(builder.button(text="⬅️", callback_data=f"shift_tz:{current_start - 3}:{setup_flag}"))
     
     # Если не в самом конце, показываем стрелку вправо
     if current_start < 9:
-        nav_buttons.append(builder.button(text="➡️", callback_data=f"shift_tz:{current_start + 3}"))
+        nav_buttons.append(builder.button(text="➡️", callback_data=f"shift_tz:{current_start + 3}:{setup_flag}"))
 
     builder.adjust(3, 3, 2)
 
